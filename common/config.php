b1bc8f6f00d98d752e36db54f818faa29c73b131
Kuba Włodarczyk <kubw19@gmail.com>
2019-10-25 19:46:19 +0200

    cos

--- common/config.php
+++ common/config.php
@@ -1,11 +1,11 @@
 <?php
 $dbHost = "localhost";
 $dbUser = "root";
-$dbPass = "KQJtHPv4mV";
-$dbName = 'dManager';
+$dbPass = "root";
+$dbName = 'dManagerNewDB';
 $mailHost = 'mail.jakubwlodarczyk.pl';
 $mailUsername = 'dmanager@jakubwlodarczyk.pl';
 $mailPassword = 'KQJtHPv4mV';
 $mailPort = 587;
-$address = 'http://dmanager.jakubwlodarczyk.pl/';
+$address = 'http://192.168.0.59:4200/';
 ?>
